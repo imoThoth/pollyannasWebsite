824b09865850084f38e895b90a85f00cd3426053
imoThoth <53401768+imoThoth@users.noreply.github.com>
2025-03-28 13:42:14 +0000

    Updated carousel

--- src/components/homePage/BootstrapCarousel.js
+++ src/components/homePage/BootstrapCarousel.js
@@ -15,14 +15,14 @@ import FishWife from '../../images/fish_wife/fishWifeCarousel.jpg';
 
 
 const images = [
-  {id: 1, src: niceWorkCarousel, design: 'Costume & Set', date: '2018', name: 'Nice Work If You Can Get It' },
-  {id: 2, src: mimiCarousel, design: 'Costume', date: '2023', name: 'Drag Mx Great Britain' },
-  {id: 3, src: oneEightyCarousel, design: 'Costume & Set', date: '2024', name: 'One Hundred and Eighty' },
-  {id: 4, src: punchDrunkCarousel, design: 'Costume', date: '2024', name: 'Enitans Game' },
-  {id: 5, src: Morroccan, design: 'Costume', date: '2021', name: 'Immersive 70s Marrakesh' },
-  {id: 6, src: hitchikersGuide, design: 'Costume', date: '2021', name: 'Immersive Hitchhikers Guide to The Galaxy' },
-  {id: 7, src: WildAnimal, design: 'Costume & Set', date: '2023', name: 'Wild Animal' },
-  {id: 8, src: FishWife, design: 'Costume & Set', date: '2021', name: 'Fish Wife' }
+  {id: 1, src: niceWorkCarousel, design: 'Theatre - Set and Costume', date: '2018', name: 'Nice Work If You Can Get It' },
+  {id: 2, src: mimiCarousel, design: 'Events - Costume Design / Making', date: '2023', name: 'Drag Mx Great Britain' },
+  {id: 3, src: oneEightyCarousel, design: 'Short Film - ‘Production Design & Costume Design', date: '2024', name: 'One Hundred and Eighty' },
+  {id: 4, src: punchDrunkCarousel, design: 'Theatre - Costume Design / Supervising', date: '2024', name: 'Enitans Game' },
+  {id: 5, src: Morroccan, design: 'Events - Costume Design / Supervising', date: '2021', name: 'Immersive 70s Marrakesh' },
+  {id: 6, src: hitchikersGuide, design: 'Events - Costume Design / Supervising / Making', date: '2021', name: 'Immersive Hitchhikers Guide to The Galaxy' },
+  {id: 7, src: WildAnimal, design: 'Short Film - Production Design & Costume Design', date: '2023', name: 'Wild Animal' },
+  {id: 8, src: FishWife, design: 'Short Film - Production Design & Costume Design', date: '2021', name: 'Fish Wife' }
 ];
 
 const BootStrapCarousel = () => {
