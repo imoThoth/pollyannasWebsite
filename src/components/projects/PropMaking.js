a4e9995410a85c4efc6ffc36b8bd3f7f20c80ca2
Popoteenie <pollyannaelstondesign@gmail.com>
2025-03-31 15:57:17 +0000

    rearranged prop photos

--- src/components/projects/PropMaking.js
+++ src/components/projects/PropMaking.js
@@ -43,49 +43,100 @@ import treeZoomed from "../../images/props/propsTreeZoomed.jpg";
 import voodooDoll from "../../images/props/propsVoodooDoll.jpg";
 import voodooStick from "../../images/props/propsVoodooStick.jpg";
 import propTree from "../../images/props/propTree.jpg";
+import propproject1 from"../../images/props/props1.jpg";
+import propproject2 from"../../images/props/props2.jpg";
+import propproject3 from"../../images/props/props3.jpg";
+import propproject4 from"../../images/props/props4.jpg";
+import propproject5 from"../../images/props/props5.jpg";
+import propproject6 from"../../images/props/props6.jpg";
+import propproject7 from"../../images/props/props7.jpg";
+import propproject8 from"../../images/props/props8.jpg";
+import propproject9 from"../../images/props/props9.jpg";
+import propproject10 from"../../images/props/props10.jpg";
+import propproject11 from"../../images/props/props11.jpg";
+import propproject12 from"../../images/props/props12.jpg";
+import propproject13 from"../../images/props/props13.jpg";
+import propproject14 from"../../images/props/props14.jpg";
+import propproject15 from"../../images/props/props15.jpg";
+import propproject16 from"../../images/props/props16.jpg";
+import propproject17 from"../../images/props/props17.jpg";
+import propproject18 from"../../images/props/props18.jpg";
+import propproject19 from"../../images/props/props19.jpg";
+import propproject20 from"../../images/props/props20.jpg";
+import propproject21 from"../../images/props/props21.jpg";
+import propproject22 from"../../images/props/props22.jpg";
+import propproject23 from"../../images/props/props23.jpg";
+import propproject24 from"../../images/props/props24.jpg";
+import propproject25 from"../../images/props/props25.jpg";
+import propproject26 from"../../images/props/props26.jpg";
+import propproject27 from"../../images/props/props27.jpg";
+import propproject28 from"../../images/props/props28.jpg";
+import propproject29 from"../../images/props/props29.jpg";
+import propproject30 from"../../images/props/props30.jpg";
+import propproject31 from"../../images/props/props31.jpg";
+import propproject32 from"../../images/props/props32.jpg";
+import propproject33 from"../../images/props/props33.jpg";
+import propproject34 from"../../images/props/props34.jpg";
+import propproject35 from"../../images/props/props35.jpg";
+import propproject36 from"../../images/props/props36.jpg";
+import propproject37 from"../../images/props/props37.jpg";
+import propproject38 from"../../images/props/props38.jpg";
+import propproject39 from"../../images/props/props39.jpg";
+import propproject40 from"../../images/props/props40.jpg";
+import propproject41 from"../../images/props/props41.jpg";
+import propproject42 from"../../images/props/props42.jpg";
+import propproject43 from"../../images/props/props43.jpg";
+import propproject44 from"../../images/props/props44.jpg";
+import propproject45 from"../../images/props/props45.jpg";
 
 
 
 const images = [
-  { id: 1, type: "Props", src: absinthe, caption: "absinthe" },
-  { id: 2, type: "Props", src: bag, caption: "bag" },
-  { id: 3, type: "Props", src: braPearls, caption: "braPearls" },
-  { id: 4, type: "Props", src: carrotFan, caption: "carrotFan" },
-  { id: 5, type: "Props", src: curtain, caption: "curtain" },
-  { id: 6, type: "Props", src: dress, caption: "dress" },
-  { id: 7, type: "Props", src: eggs, caption: "eggs" },
-  { id: 8, type: "Props", src: featherMask, caption: "featherMask" },
-  { id: 9, type: "Props", src: featherMaskZoomed, caption: "featherMaskZoomed" },
-  { id: 10, type: "Props", src: flowers, caption: "flowers" },
-  { id: 11, type: "Props", src: funerals, caption: "funerals" },
-  { id: 12, type: "Props", src: funeralZoom, caption: "funeralZoom" },
-  { id: 13, type: "Props", src: iceCream, caption: "iceCream" },
-  { id: 14, type: "Props", src: iceCreamAgain, caption: "iceCreamAgain" },
-  { id: 15, type: "Props", src: machineMotherBoard, caption: "machineMotherBoard" },
-  { id: 16, type: "Props", src: machineRadar, caption: "machineRadar" },
-  { id: 17, type: "Props", src: macineRadarZoomed, caption: "macineRadarZoomed" },
-  { id: 18, type: "Props", src: magus, caption: "magus" },
-  { id: 19, type: "Props", src: medusa, caption: "medusa" },
-  { id: 20, type: "Props", src: medusaPerson, caption: "medusaPerson" },
-  { id: 21, type: "Props", src: money, caption: "money" },
-  { id: 22, type: "Props", src: pearlHair, caption: "pearlHair" },
-  { id: 23, type: "Props", src: rat, caption: "rat" },
-  { id: 24, type: "Props", src: ratAgain, caption: "ratAgain" },
-  { id: 26, type: "Props", src: sFlag, caption: "sFlag" },
-  { id: 27, type: "Props", src: sShield, caption: "sShield" },
-  { id: 28, type: "Props", src: star, caption: "star" },
-  { id: 29, type: "Props", src: swimmingCostume, caption: "swimmingCostume" },
-  { id: 30, type: "Props", src: sylkoRope, caption: "sylkoRope" },
-  { id: 31, type: "Props", src: treeStage, caption: "treeStage" },
-  { id: 32, type: "Props", src: treeZoomed, caption: "treeZoomed" },
-  { id: 33, type: "Props", src: voodooDoll, caption: "voodooDoll" },
-  { id: 34, type: "Props", src: voodooStick, caption: "voodooStick" },
-  { id: 35, type: "Props", src: smallShoes, caption: "smallShoes" },
-  { id: 36, type: "Props", src: shoes, caption: "shoes" },
-  { id: 37, type: "Props", src: dollsClothed, caption: "dollsClothed" },
-  { id: 38, type: "Props", src: dolls, caption: "l" },
-  { id: 39, type: "Props", src: dollManClothed, caption: "dollManClothed" },
-  { id: 40, type: "Props", src: dollBlondeClothed, caption: "dollBlondeClothed" },
+  { id: 1, type: "Props", src: propproject1, caption: "absinthe" },
+  { id: 2, type: "Props", src: propproject2, caption: "bag" },
+  { id: 3, type: "Props", src: propproject3, caption: "braPearls" },
+  { id: 4, type: "Props", src: propproject4, caption: "carrotFan" },
+  { id: 5, type: "Props", src: propproject5, caption: "curtain" },
+  { id: 6, type: "Props", src: propproject6, caption: "dress" },
+  { id: 7, type: "Props", src: propproject7, caption: "eggs" },
+  { id: 8, type: "Props", src: propproject8, caption: "featherMask" },
+  { id: 9, type: "Props", src: propproject9, caption: "featherMaskZoomed" },
+  { id: 10, type: "Props", src: propproject10, caption: "flowers" },
+  { id: 11, type: "Props", src: propproject11, caption: "funerals" },
+  { id: 12, type: "Props", src: propproject12, caption: "funeralZoom" },
+  { id: 13, type: "Props", src: propproject13, caption: "iceCream" },
+  { id: 14, type: "Props", src: propproject14, caption: "iceCreamAgain" },
+  { id: 15, type: "Props", src: propproject15, caption: "machineMotherBoard" },
+  { id: 16, type: "Props", src: propproject16, caption: "machineRadar" },
+  { id: 17, type: "Props", src: propproject17, caption: "macineRadarZoomed" },
+  { id: 18, type: "Props", src: propproject18, caption: "magus" },
+  { id: 19, type: "Props", src: propproject19, caption: "medusa" },
+  { id: 20, type: "Props", src: propproject20, caption: "medusaPerson" },
+  { id: 21, type: "Props", src: propproject21, caption: "money" },
+  { id: 22, type: "Props", src: propproject22, caption: "pearlHair" },
+  { id: 23, type: "Props", src: propproject23, caption: "rat" },
+  { id: 24, type: "Props", src: propproject24, caption: "ratAgain" },
+  { id: 26, type: "Props", src: propproject25, caption: "sFlag" },
+  { id: 27, type: "Props", src: propproject26, caption: "sShield" },
+  { id: 28, type: "Props", src: propproject27, caption: "star" },
+  { id: 29, type: "Props", src: propproject28, caption: "swimmingCostume" },
+  { id: 30, type: "Props", src: propproject29, caption: "sylkoRope" },
+  { id: 31, type: "Props", src: propproject30, caption: "treeStage" },
+  { id: 32, type: "Props", src: propproject31, caption: "treeZoomed" },
+  { id: 33, type: "Props", src: propproject32, caption: "voodooDoll" },
+  { id: 34, type: "Props", src: propproject33, caption: "voodooStick" },
+  { id: 35, type: "Props", src: propproject34, caption: "smallShoes" },
+  { id: 36, type: "Props", src: propproject35, caption: "shoes" },
+  { id: 37, type: "Props", src: propproject36, caption: "dollsClothed" },
+  { id: 38, type: "Props", src: propproject37, caption: "l" },
+  { id: 39, type: "Props", src: propproject38, caption: "dollManClothed" },
+  { id: 40, type: "Props", src: propproject39, caption: "dollBlond blah" },
+  { id: 41, type: "Props", src: propproject40, caption: "dollBl pajajajad" },
+  { id: 42, type: "Props", src: propproject41, caption: "dollBlondefnfnfnnfeed" },
+  { id: 43, type: "Props", src: propproject42, caption: "dollBlondeCdjjdjdjdthed" },
+  { id: 44, type: "Props", src: propproject43, caption: "dollBlondedjdjdlothed" },
+  { id: 45, type: "Props", src: propproject44, caption: "dollBlondjdjdjdClothed" },
+  { id: 46, type: "Props", src: propproject45, caption: "dollBlonddnndndClothed" },
 
   ]
 
